83e2c4985fc21d3fad3efcb55d2fa694a3f28623
rahul <rahul97164@gmail.com>
2020-05-04 09:18:54 +0530

    array

--- ConvertArray.java
+++ ConvertArray.java
@@ -1,7 +1,7 @@
 package ArrayProblems;
 
 public class ConvertArray {
-	private static String[] transformArr(String[] arr, int n)
+	private static String[] transformArr(String[] arr, int n, int nr)
 	{
 		int len = arr.length;
 		String[] newArr = new String[len]; // new array which stores transformed array
@@ -9,17 +9,18 @@ public class ConvertArray {
 		{
 			int t = (i+n) / n;
 			int row = t - 1;
-			int col = i - (row*n);
-			int newIndex = col*n + row; // bcoz (row*n + col = i)
+			int col = i - (row*n); // finding col no. considering RMO (row  major order)
+			int newIndex = col*nr + row; // finding newIndex based on CMO (column major order)
 			newArr[newIndex] = arr[i];
 		}
 		return newArr;
 	}
 	public static void main(String args[])
 	{
-		String a[] = {"a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"};
-		int N = 3; // a1, a2, ......., aN
-		String rslt[] = ConvertArray.transformArr(a, N);
+		String a[] = {"a1", "a2", "a3", "a4", "b1", "b2", "b3", "b4", "c1", "c2", "c3", "c4"}; // this can be visualized as 3x4 2D array
+		int N = 4; // a1, a2, ......., aN
+		int nr = 3; // no. of rows i.e different variables list, like here run of a's, b's and c's => a,b,c gives 3 rows
+		String rslt[] = ConvertArray.transformArr(a, N, nr);
 		
 		System.out.println("Input Array:");
 		for(int i=0; i<a.length; i++)
